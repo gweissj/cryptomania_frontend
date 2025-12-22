6f2232468a2c9bb45028afcb8b9ab81cc09b95fb
Alina <kwixkazao@gmail.com>
2025-12-22 11:49:23 +0700

    fix: change the server connection

--- lib/src/core/app_config.dart
+++ lib/src/core/app_config.dart
@@ -3,6 +3,6 @@ class AppConfig {
 
   static const backendBaseUrl = String.fromEnvironment(
     'BACKEND_BASE_URL',
-    defaultValue: 'http://10.0.2.2:8000/',
+    defaultValue: 'http://10.0.2.2/',
   );
 }
